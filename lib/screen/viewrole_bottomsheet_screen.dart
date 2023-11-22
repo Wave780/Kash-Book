22bf7eaf64eb66493c9a3cebcebe40dadd6b7a18
Wave780 <jonahemma12@gmail.com>
2023-11-22 15:19:39 +0100

    update

--- lib/screen/viewrole_bottomsheet_screen.dart
+++ lib/screen/viewrole_bottomsheet_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:kashbook_app/utils/extension.dart';
-import 'package:kashbook_app/utils/utils.dart';
 import 'package:kashbook_app/widgets/roleinfo_container.dart';
 
 class ViewRoleBottomSheetScreen extends StatefulWidget {
@@ -15,7 +13,6 @@ class ViewRoleBottomSheetScreen extends StatefulWidget {
 class _ViewRoleBottomSheetScreenState extends State<ViewRoleBottomSheetScreen> {
   @override
   Widget build(BuildContext context) {
-    final colors = context.colorScheme;
     return DefaultTabController(
       length: 3,
       child: Column(
@@ -44,82 +41,70 @@ class _ViewRoleBottomSheetScreenState extends State<ViewRoleBottomSheetScreen> {
               child: TabBarView(children: [
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Container(
-                decoration: BoxDecoration(
-                    border: Border.all(width: 1.0, color: colors.primary)),
-                child: Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: RoleContainerWidget(
-                    headerTitle1: 'Permissions',
-                    headerTitle2: '',
-                    detail: 'Full access to all record of each account',
-                    icon1: Icons.check_circle,
-                    icon3: Icons.check_circle,
-                    color1: Colors.green.shade700,
-                    color3: Colors.green.shade700,
-                    detail2: 'Full access to record settings',
-                    detail3: 'Add/remove members in  record Account',
-                    detail4: '',
-                    detail5: '',
-                    detail6: '',
-                  ),
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: RoleContainerWidget(
+                  headerTitle1: 'Permissions',
+                  headerTitle2: '',
+                  detail: 'Full access to all record of each account',
+                  icon1: Icons.check_circle,
+                  icon3: Icons.check_circle,
+                  color1: Colors.green.shade700,
+                  color3: Colors.green.shade700,
+                  detail2: 'Full access to record settings',
+                  detail3: 'Add/remove members in  record Account',
+                  detail4: '',
+                  detail5: '',
+                  detail6: '',
                 ),
               ),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Container(
-                decoration: BoxDecoration(
-                    border: Border.all(width: 1.0, color: colors.primary)),
-                child: Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: RoleContainerWidget(
-                    headerTitle1: 'Permissions',
-                    headerTitle2: 'Restrictions',
-                    detail: 'Full access to all record in this account',
-                    icon1: Icons.check_circle,
-                    icon3: Icons.check_circle,
-                    icon4: Icons.cancel,
-                    icon5: Icons.cancel,
-                    color1: Colors.green.shade700,
-                    color3: Colors.green.shade700,
-                    color4: Colors.red.shade700,
-                    color5: Colors.red.shade700,
-                    detail2: 'Full access to record settings',
-                    detail3: 'Add/remove members in  record Account',
-                    detail4: "Can't delete account",
-                    detail5: "Can't remove onwer from account",
-                    detail6: '',
-                  ),
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: RoleContainerWidget(
+                  headerTitle1: 'Permissions',
+                  headerTitle2: 'Restrictions',
+                  detail: 'Full access to all record in this account',
+                  icon1: Icons.check_circle,
+                  icon3: Icons.check_circle,
+                  icon4: Icons.cancel,
+                  icon5: Icons.cancel,
+                  color1: Colors.green.shade700,
+                  color3: Colors.green.shade700,
+                  color4: Colors.red.shade700,
+                  color5: Colors.red.shade700,
+                  detail2: 'Full access to record settings',
+                  detail3: 'Add/remove members in  record Account',
+                  detail4: "Can't delete account",
+                  detail5: "Can't remove onwer from account",
+                  detail6: '',
                 ),
               ),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Container(
-                decoration: BoxDecoration(
-                    border: Border.all(width: 1.0, color: colors.primary)),
-                child: Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: RoleContainerWidget(
-                    headerTitle1: 'Permissions ',
-                    headerTitle2: 'Restrictions',
-                    detail: 'Limited access to authorized account',
-                    icon1: Icons.check_circle,
-                    icon4: Icons.cancel,
-                    icon5: Icons.cancel,
-                    icon6: Icons.cancel,
-                    color1: Colors.green.shade700,
-                    color4: Colors.red.shade700,
-                    color5: Colors.red.shade700,
-                    color6: Colors.red.shade700,
-                    detail2:
-                        'Owner/Partner can assign Modertor, Viewer or\n Operator to staffin any Account',
-                    detail3: '',
-                    detail4: 'No access to account they are not authorized',
-                    detail5: 'No access to record setting',
-                    detail6: 'No option to delete books',
-                  ),
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: RoleContainerWidget(
+                  headerTitle1: 'Permissions ',
+                  headerTitle2: 'Restrictions',
+                  detail: 'Limited access to authorized account',
+                  icon1: Icons.check_circle,
+                  icon4: Icons.cancel,
+                  icon5: Icons.cancel,
+                  icon6: Icons.cancel,
+                  color1: Colors.green.shade700,
+                  color4: Colors.red.shade700,
+                  color5: Colors.red.shade700,
+                  color6: Colors.red.shade700,
+                  detail2:
+                      'Owner/Partner can assign Modertor, Viewer or\n Operator to staffin any Account',
+                  detail3: '',
+                  detail4: 'No access to account they are not authorized',
+                  detail5: 'No access to record setting',
+                  detail6: 'No option to delete books',
                 ),
               ),
             ),
@@ -133,4 +118,3 @@ class _ViewRoleBottomSheetScreenState extends State<ViewRoleBottomSheetScreen> {
     );
   }
 }
-
